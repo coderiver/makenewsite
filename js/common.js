f2825e61e669484f4c64db3aeb80dd2fc5736ee2
Yevgen <godun.yevgen@gmail.com>
2012-10-31 17:41:55 +0200

    готово, но зараза ие не грузит шрифты с гугла - нада лечить

--- js/common.js
+++ js/common.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+// cycle 
+ $('.scroll').cycle({ 
+    fx:     'fade',
+    timeout: 5000,
+    speed: 700
+});
 
+$(".popup").hide();
+$(".popup-wrap").hide();
+$(".header__contacts a").click(function(){
+    $(".popup-wrap").fadeIn();
+    $(".popup_feedback").fadeIn();
+    return false;
+});
+$(".btn-new-site").click(function(){
+    $(".popup-wrap").fadeIn();
+    $(".popup_order").fadeIn();
+    return false;
+});
+$(".popup-wrap, .popup__close").click(function(){
+    $(".popup-wrap").fadeOut();
+    $(".popup").fadeOut();
+
+});
 
 })
